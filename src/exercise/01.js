ef6ad5a3943adb34aad66b72c894c720a27a8a1d
abigailjohnson-toast <abigail.johnson@toasttab.com>
2023-01-23 15:58:26 -0500

    exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,12 +3,15 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initalName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name , setName] = React.useState(initalName)
 
-  function handleChange(event) {
+  return 
+
+  function handleChanged(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
@@ -23,7 +26,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initalName = "Izzy"/>
 }
 
 export default App
